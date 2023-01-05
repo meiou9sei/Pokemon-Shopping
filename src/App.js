47b62568fc3a52043be40625b39d86dc0b840153
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-05 15:15:57 -0500

    🚧 create navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Footer />
     </div>
   );
