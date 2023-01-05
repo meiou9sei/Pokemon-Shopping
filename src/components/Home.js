2d55530219c6e4423ea7e9c951946fa147fb4bd7
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-05 15:58:28 -0500

    ✨ create home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='homepage'>
+      <div className='hero'>
+        <h1>Welcome to the (broke) Pokemart</h1>
+        <p>your (future) store for all pokemon needs</p>
+      </div>
+      <p>
+        we're a bit broke right now. can't afford graphic designer. or stock.
+        that's why we're selling pokemon we catch outside the store. then we can
+        afford to sell pokemon-care goods later
+      </p>
+    </div>
+  );
 }
