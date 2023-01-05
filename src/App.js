10332a0aa607ab2a0365b8d6684f1324e95b6e4d
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-05 15:21:16 -0500

    🚧 install react-router-dom, create other page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Home />
+      <Store />
+      <Cart />
       <Footer />
     </div>
   );
