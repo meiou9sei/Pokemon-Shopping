97e20e027a10cba4a50361c133e3beccd4fbe934
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-05 15:49:33 -0500

    💄 basic styling on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,17 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <li>
-        <Link to='/'>Home</Link>
-        <Link to='/store'>Store</Link>
-        <Link to='/cart'>Cart</Link>
-      </li>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/store'>Store</Link>
+        </li>
+        <li>
+          <Link to='/cart'>Cart</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
