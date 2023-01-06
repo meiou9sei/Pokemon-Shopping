1cf98df54d34a6a5e705c742bce9accbafadeba4
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-06 16:29:37 -0500

    💄 CSS update for page and setting up empty cart

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function Checkout({ cart }) {
+  console.log(cart);
   return (
     <section>
       <h1>Your Cart</h1>
       <p>here's all the pokemon you're gonna buy</p>
-      <div className='checkout-cart'></div>
+      <div className='checkout-cart'>
+        {cart.length === 0 && <p>your cart is empty. go add some pokemon</p>}
+      </div>
     </section>
   );
 }
