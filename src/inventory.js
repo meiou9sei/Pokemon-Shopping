96a85ede5c532155b4182b028de71ce2114d1548
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-06 12:50:31 -0500

    🚧 create temporary inventory data

---
+++ src/inventory.js
@@ -0,0 +1,19 @@
+const inventory = [
+  {
+    name: "shiba inu",
+    price: 299,
+    image: "https://images.dog.ceo/breeds/shiba/shiba-1.jpg",
+  },
+  {
+    name: "collie",
+    price: 399,
+    image: "https://images.dog.ceo/breeds/collie-border/n02106166_331.jpg",
+  },
+  {
+    name: "tervuren",
+    price: 399,
+    image: "https://images.dog.ceo/breeds/tervuren/maverick.jpg",
+  },
+];
+
+export default inventory;
