7b1973621849db00c396effe5e5ac54a5a77c492
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-02-16 13:25:45 -0500

    ♻️ remove image link

--- src/useFetchInventory.js
+++ src/useFetchInventory.js
@@ -1,41 +1,17 @@
 import { useState, useEffect } from "react";
-
-const CURRMAXPOKEMON = 1008;
-const AMOUNTTOFETCH = 151;
+import PokemonDB from './data/PokemonDb.json'
 
 const useFetchInventory = () => {
   const [storeInventory, setStoreInventory] = useState([]);
   const [isInventoryLoaded, setIsInventoryLoaded] = useState(false);
 
   useEffect(function fetchInventory() {
-    fetchData(AMOUNTTOFETCH);
+    fetchData(PokemonDB.pokemon);
   }, []);
 
-  async function fetchData(amountToFetch) {
-    const dataArray = [];
-    for (let pokemonId = 1; pokemonId < amountToFetch + 1; pokemonId++) {
-      const data = await fetchPokemon(pokemonId);
-      dataArray.push(data);
-    }
-    setStoreInventory(dataArray);
+  function fetchData(listing) {
+    setStoreInventory(listing);
     setIsInventoryLoaded(true);
-  }
-
-  // fetches pokemon name and image
-  async function fetchPokemon(pokemonId) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    if (!res.ok) {
-      throw new Error("cannot fetch data");
-    }
-    const data = await res.json();
-    const dataObj = {
-      id: pokemonId,
-      name: data.name,
-      image: data.sprites.other["official-artwork"].front_default,
-      price: Math.floor(Math.random() * 999),
-      type: [...data.types.map((index) => index.type.name)],
-    };
-    return dataObj;
   }
 
   return {
