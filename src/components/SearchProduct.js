5eb1551fe5f8790c5fff61065ebd981050a1367c
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-02-19 21:01:14 -0500

    ⚰️ remove unneeded import

--- src/components/SearchProduct.js
+++ src/components/SearchProduct.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import ProductDisplay from "./ProductDisplay";
-import { Link } from "react-router-dom";
 
 export const SearchProduct = ({
   isInventoryLoaded,
