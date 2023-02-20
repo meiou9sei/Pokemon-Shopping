a69c6937bfee5420cda24cfa7c77fc563073afda
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-02-19 20:33:40 -0500

    ✨ display results count for searchTerm

--- src/components/SearchProduct.js
+++ src/components/SearchProduct.js
@@ -8,15 +8,24 @@ export const SearchProduct = ({
   setIsSearchBarVisible,
 }) => {
   const [sortedInventory, setSortedInventory] = useState([]);
+  const [filteredSortedInventory, setFilteredSortedInventory] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    setSortedInventory(inventory);
-  }, [isInventoryLoaded]);
 
   useEffect(() => {
     setSortedInventory(inventory.sort(sortProductsAlphabetically));
   }, [isInventoryLoaded]);
+
+  useEffect(() => {
+    setFilteredSortedInventory(
+      sortedInventory.filter((item) => {
+        if (searchTerm == "") {
+          return;
+        } else if (item.name.includes(searchTerm.toLowerCase())) {
+          return item;
+        }
+      })
+    );
+  }, [searchTerm]);
 
   function sortProductsAlphabetically(a, b) {
     if (a.name < b.name) {
@@ -46,33 +55,26 @@ export const SearchProduct = ({
       />
       <div className='search-results-container'>
         <div className='search-bar-top'>
-          <p className='results-count'>{} result(s)</p>
+          <p className='results-count'>
+            {filteredSortedInventory.length} result(s)
+          </p>
         </div>
         <div className='search-bar-bottom'>
           <ul className='search-results'>
-            {sortedInventory
-              .filter((item) => {
-                if (searchTerm == "") {
-                  return;
-                } else if (item.name.includes(searchTerm.toLowerCase())) {
-                  return item;
-                }
-              })
-              .slice(0, 4)
-              .map((item) => {
-                return (
-                  <li
-                    className='result-product-wrapper'
-                    key={item.id}
-                    onClick={clearClickToSearch}
-                  >
-                    <ProductDisplay
-                      className='search-result-item'
-                      product={item}
-                    />
-                  </li>
-                );
-              })}
+            {filteredSortedInventory.slice(0, 4).map((item) => {
+              return (
+                <li
+                  className='result-product-wrapper'
+                  key={item.id}
+                  onClick={clearClickToSearch}
+                >
+                  <ProductDisplay
+                    className='search-result-item'
+                    product={item}
+                  />
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
